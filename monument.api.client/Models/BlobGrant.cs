039e77d730d2aba06b1bc407b7f3fda311234cce
aluitink <aluitink@gmail.com>
2024-08-25 15:44:51 -0500

    moving page model to its own file

--- monument.api.client/Models/BlobGrant.cs
+++ monument.api.client/Models/BlobGrant.cs
@@ -1,15 +1,5 @@
 ﻿namespace monument.api.client.Models
 {
-    public class PageSection
-    {
-        public string Body { get; set; }
-    }
-    public class Page
-    {
-        public string Name { get; set; }
-        ICollection<PageSection> Sections { get; set; }
-
-    }
     public class BlobGrant
     {
         public string BlobId { get; set; }
