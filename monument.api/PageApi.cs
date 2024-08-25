eb6696be1debac7d1a46ecfa4fe2310314bfb65a
aluitink <aluitink@gmail.com>
2024-08-25 11:52:33 -0500

    Add Page Api/Update Schema

---
+++ monument.api/PageApi.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.OpenApi.Models;
+using monument.api.client.Models;
+using System.Net;
+
+namespace monument.api
+{
+    public class PageApi: ApiBase
+    {
+        [Function(nameof(GetPagesAsync))]
+        [OpenApiOperation(operationId: nameof(GetPagesAsync), tags: new[] { nameof(PageApi) })]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Description = "The NotFound response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<Page>), Description = "The OK response")]
+        public async Task<IActionResult> GetPagesAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "pages")] HttpRequest req)
+        {
+            return new OkObjectResult(new List<Page>());
+
+        }
+        [Function(nameof(GetPageAsync))]
+        [OpenApiOperation(operationId: nameof(GetPageAsync), tags: new[] { nameof(PageApi) })]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Description = "The NotFound response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Page), Description = "The OK response")]
+        public async Task<IActionResult> GetPageAsync(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "pages/{pageName}")] HttpRequest req, string pageName)
+        {
+            return new OkObjectResult(new Page() { Name = pageName });
+        }
+
+        [Function(nameof(SetPageAsync))]
+        [OpenApiOperation(operationId: nameof(SetPageAsync), tags: new[] { nameof(PageApi) })]
+        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(Page), Required = true)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Page), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Description = "The BadRequest response")]
+        public async Task<IActionResult> SetPageAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", "put", Route = "pages")] HttpRequest req)
+        {
+            var pageToSet = ObjectFromRequestAsync<Page>(req);
+            if (pageToSet != null)
+                return new OkObjectResult(pageToSet);
+            return new BadRequestObjectResult("Bad Request");
+        }
+
+        [Function(nameof(DeletePageAsync))]
+        [OpenApiOperation(operationId: nameof(DeletePageAsync), tags: new[] { nameof(PageApi) })]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.OK, Description = "The OK response")]
+        public async Task<IActionResult> DeletePageAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "pages/{pageName}")] HttpRequest req, string pageName)
+        {
+            return new OkResult();
+        }
+    }
+}
