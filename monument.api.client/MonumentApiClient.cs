3da4727aa2438812b46c5d8597cd445ab10bb0c5
aluitink <aluitink@gmail.com>
2024-08-25 15:43:31 -0500

    Update schema - configure serializer

--- monument.api.client/MonumentApiClient.cs
+++ monument.api.client/MonumentApiClient.cs
@@ -1,7 +1,16 @@
-﻿namespace monument.api.client
+﻿using System.Text.Json;
+
+namespace monument.api.client
 {
     public partial class MonumentApiClient
     {
+        partial void UpdateJsonSerializerSettings(JsonSerializerOptions settings)
+        {
+            settings.PropertyNameCaseInsensitive = true;
+            settings.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            settings.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            settings.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+        }
 
     }
 }
